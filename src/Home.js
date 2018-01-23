2a6bb468f33988c4885cdcc55869d08ecbba6d3d
Christopher Tran <christopher.long.tran@gmail.com>
2018-01-23 14:20:55 -0500

    created home page

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class Home extends Component {
+  render () {
+    return (
+      <div className='home'>
+        <a class='pic' href='/tasks'><img src='http://socialventurepartners.s3-us-west-2.amazonaws.com/www.socialventurepartners.org/sites/46/2015/11/Welcome.jpg' alt='Welcome' /></a>
+      </div>
+    )
+  }
+}
+
+export default Home
