581878e639edc73d72b283372e9313c9f394c93b
Christopher Tran <christopher.long.tran@gmail.com>
2018-01-23 14:27:25 -0500

    recreated navbar and called it in App.js

---
+++ src/NavBar.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react'
+
+class NavBar extends Component {
+  render () {
+    return (
+      <div>
+        <nav>
+          <div class='nav-wrapper'>
+            <a href='/#' class='brand-logo'>Chris' Tasks</a>
+            <ul class='right hide-on-med-and-down'>
+              <li><a href='/tasks'>Tasks List</a></li>
+              <li><a href='/about'>About</a></li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    )
+  }
+}
+
+export default NavBar
