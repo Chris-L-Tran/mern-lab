422d460da96d613b5ad262f9b98dd5adcc603cce
Christopher Tran <christopher.long.tran@gmail.com>
2018-01-23 16:38:48 -0500

    Added an addtask component

---
+++ src/AddTask.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react'
+import {Route} from 'react-router-dom'
+import {API} from './urls'
+import axios from 'axios'
+
+class AddTask extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      title: ''
+    }
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  handleSubmit (e) {
+    e.preventDefault()
+    axios.post(API, this.state)
+    .catch(err => console.log(err))
+    .then(res => {
+      this.setState({...res.data})
+      this.props.history.push('/')
+    })
+  }
+  render () {
+    return (
+      <div>
+        <h2>Add a new Task </h2>
+        <form onSubmit={(e) => this.handleSubmit(e)}>
+          <input name='title' placeholder='title' onChange={(e) => this.handleChange(e)} />
+          <input className='submit' type='submit' value='Add' />
+        </form>
+      </div>
+    )
+  }
+}
